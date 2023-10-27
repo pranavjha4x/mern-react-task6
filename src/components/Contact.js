20eeb35701bbd9b17308db3d38bdf349f95682cb
pranavjha4x <144226113+pranavjha4x@users.noreply.github.com>
2023-10-27 12:40:13 +0530

    second commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,8 +5,8 @@ function Contact() {
   return (
     <section id="contact">
       <h2>Contact</h2>
-      <p>I'm thrilled that you've taken the time to explore my portfolio. Whether you're interested in collaborating on a project, have a question, or just want to connect, I'd love to hear from you. Feel free to reach out using any of the following methods:</p>
-      <p>Email: vidhianand0211@gmail.com</p>
+      <p>Feel Free to Contact me</p>
+      <p>Email: pranavjha4x@gmail.com</p>
     </section>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,23 +6,23 @@ function Projects() {
     <section id="projects">
       <h2>Projects</h2>
       <div className="project">
-        <h3>Online Furniture Store</h3>
+        <h3>Online Attendance System</h3>
         <p>
-          Developed a full-stack e-commerce website for a furniture store using MERN (MongoDB, Express, React, Node.js) stack. Implemented user authentication, product listings, shopping cart, and payment processing. <a href="link-to-project">View Project</a>
+          Developed a full-stack School Attendance System for a Institutions using Pyhton <a href="link-to-project">View Project</a>
         </p>
       </div>
 
       <div className="project">
-        <h3>Social Media Platform</h3>
+        <h3>Anime Recommendation System</h3>
         <p>
-          Created a social media platform from scratch using MEAN (MongoDB, Express, Angular, Node.js) stack. Implemented features like user profiles, posts, comments, and real-time messaging. <a href="link-to-project">View Project</a>
+          Created a anime recommendation platform from scratch <a href="link-to-project">View Project</a>
         </p>
       </div>
 
       <div className="project">
-        <h3>Task Management Application</h3>
+        <h3>Quiz Website</h3>
         <p>
-          Designed and built a task management web application with a responsive front-end using React and a back-end API with Node.js and Express. Users can create, assign, and track tasks. <a href="link-to-project">View Project</a>
+          Designed and built a quiz application with a responsive front-end using html and a back-end my sql. Users can create, assig quizes. <a href="link-to-project">View Project</a>
         </p>
       </div>
 
